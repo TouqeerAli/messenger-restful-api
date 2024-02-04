00e11a6cc6ff3edd6aef2ea83f710cbd19e1d53b
TouqeerAli <engr.touqeerali@gmail.com>
2024-02-04 17:49:45 +0500

    add Profile resources and service

--- src/main/java/com/restfulApi/messenger/database/DatabaseClass.java
+++ src/main/java/com/restfulApi/messenger/database/DatabaseClass.java
@@ -8,13 +8,13 @@ import com.restfulApi.messenger.model.Profile;
 
 public class DatabaseClass {
 	private static Map<Long, Message> messages = new HashMap<Long, Message>();
-	private static Map<Long, Profile> profiles = new HashMap<Long, Profile>();
+	private static Map<String, Profile> profiles = new HashMap<String, Profile>();
 	
 	public static Map<Long,Message> getMessages(){
 		return messages;
 	
 	}
-	public static Map<Long,Profile> getProfile(){
+	public static Map<String,Profile> getProfile(){
 		return profiles;
 	}
 	
---
+++ src/main/java/com/restfulApi/messenger/service/ProfileService.java
@@ -0,0 +1,46 @@
+package com.restfulApi.messenger.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.restfulApi.messenger.database.DatabaseClass;
+import com.restfulApi.messenger.model.Profile;
+import com.restfulApi.messenger.model.Profile;
+
+public class ProfileService {
+private Map<String, Profile> profiles = DatabaseClass.getProfile();
+	
+	public ProfileService() {
+		profiles.put("Touqeer", new Profile(1,"touqeer","Touqeer","Ali"));
+		
+	}
+	
+	public List<Profile> getAllProfiles(){
+		return new ArrayList<Profile>(profiles.values());
+		
+		
+	}
+	public Profile getProfile(String profileName) {
+		return profiles.get(profileName);
+		
+	}
+	public Profile addProfile(Profile profile) {
+		profile.setId(profiles.size()+1);
+		profiles.put(profile.getProfileName(), profile);
+		return profile;
+	}
+	
+	public Profile updateProfile(Profile profile) {
+		if(profile.getProfileName().isEmpty()) {
+			return null;
+		}
+		profiles.put(profile.getProfileName(),profile);
+		return profile;
+	}
+	public Profile removeMessgae(String profileName) {
+		return profiles.remove(profileName);
+	}
+}
+
+
