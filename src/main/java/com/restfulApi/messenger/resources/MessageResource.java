d399df74ac7d8a638d5f39165ac2f73709b8abde
TouqeerAli <engr.touqeerali@gmail.com>
2024-02-04 16:57:33 +0500

    add update and delete resource

--- src/main/java/com/restfulApi/messenger/resources/MessageResource.java
+++ src/main/java/com/restfulApi/messenger/resources/MessageResource.java
@@ -4,8 +4,10 @@ import com.restfulApi.messenger.model.Message;
 import com.restfulApi.messenger.service.MessageService;
 
 import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.DELETE;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.POST;
+import jakarta.ws.rs.PUT;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.Produces;
@@ -20,7 +22,6 @@ public class MessageResource {
 	
 	
 	@GET
-	@Path("/getMessage")
 	@Produces(MediaType.APPLICATION_JSON)
 	public List<Message> getMessages() {
 		return messageService.getAllMessages();
@@ -40,4 +41,22 @@ public class MessageResource {
 		return messageService.addMessage(message);
 	}
 	
+	@PUT
+	@Path("/{messageId}")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Message updateMessage(@PathParam("messageId") long id, Message message) {
+		message.setId(id);
+		return messageService.updateMessage(message);
+		
+	}	
+	
+	
+	@DELETE
+	@Path("/{messageId}")
+	public void deleteMessage(@PathParam("messageId") long id) {
+		
+		 messageService.removeMessgae(id);
+		
+	}
 }
